b222458cc2da1d8a2d7cb1db8b04c50301f1f50d
tom hikari <tomprescott1565@gmail.com>
2020-06-12 17:04:56 +0100

    structure

---
+++ lib/screen/landingScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/screen/playerScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlayerScreen extends StatefulWidget {
+  @override
+  _PlayerScreenState createState() => _PlayerScreenState();
+}
+
+class _PlayerScreenState extends State<PlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
